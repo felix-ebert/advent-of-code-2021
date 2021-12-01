1b9f29830fc3ae347270ae4c4a0cf3baa3b166d8
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2021-12-01 15:18:24 +0000

    Add Day 1: Sonar Sweep

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -1,17 +1,23 @@
 package days
 
+// source: https://todd.ginsberg.com/post/advent-of-code/2021/day1/
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        // docs: https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/zip-with-next.html
+        return inputList
+            .map { it.toInt() }
+            .zipWithNext()
+            .count { it.first < it.second }
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        // docs: https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/windowed.html
+        return inputList
+            .map { it.toInt() }
+            .windowed(3, 1)
+            .map { it.sum() }
+            .zipWithNext()
+            .count { it.first < it.second }
     }
 }
